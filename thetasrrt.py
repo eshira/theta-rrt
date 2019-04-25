4c351ee76f11c94b2a1453f6eb9e3e8bbe35f36e
Shira <shira.xiv@gmail.com>
2019-04-25 16:33:18 -0400

    implemented A*

--- thetasrrt.py
+++ thetasrrt.py
@@ -2,10 +2,125 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 from PIL import Image
+from queue import PriorityQueue
+import math
+import itertools
+
+def heuristic(node, goal):
+	# Wrapper for convenience, easy to swap out what to use here
+	return L2norm(node, goal)
+
+def L2norm(node0, node1):
+	return math.sqrt(pow((node1[0]-node0[0]),2)+pow((node1[1]-node0[1]),2))
+
+def valid(node):
+	# Node not out of bounds
+	if ( (node[0]<0) or (node[1]<0) ):
+		return False
+	elif ( (node[0]>=imarray.shape[0]) or (node[1]>=imarray.shape[1]) ):
+		return False
+	else:
+		return True
+
+def freespace(node):
+	if imarray[node[1]][node[0]]:
+		return True
+	else:
+		return False
+
+def getneighbors(node):
+	listofneighbors = []
+	# Get the neighbors of the node
+	for delta in itertools.product([-1,0,1],repeat=2):
+		if (delta!=(0,0)):
+			candidate = np.subtract(node,delta)
+			if(freespace(candidate) and valid(candidate)):
+				#valid free neighbor
+				listofneighbors.append(tuple(candidate))
+	return listofneighbors
+
+def reconstruct(node,pathmap):
+	path = []
+	while(True):
+		try:
+			path.append(node)
+			node = pathmap[node];
+		except:
+			break
+	return path[::-1]
+
+def astar(start,goal): # Pass in two tuples in the form (x,y)
+	
+	if not valid(start) or not valid(goal):
+		print("Start or goal is not valid. Error.")
+		return False
+	if not freespace(start) or not freespace(goal):
+		print("Start or goal is inside an obstacle. Error.")
+		return False
+
+	# Set up data structures
+	closedSet = set([]) # The set of nodes already evaluated
+	openPQ = PriorityQueue() # Candidates for evaluation
+	openSet = set([])
+	cameFrom = {} # Keys are nodes, values are the predecessor node
+	gScore = {} # cost from start to node structure
+
+	# Initialize with start node
+	openPQ.put((heuristic(start,goal),start)) #put the start into the open pq
+	openSet.add(start) #and into the open set
+	gScore[start]=0 # start to start costs 0
+
+	# loop until found goal or openPQ is emtpy
+	while not openPQ.empty():
+		# Get the lowest priority node, remove from openset and add to closed
+		current = openPQ.get()[1]
+		if current in closedSet:
+			# This is here because we leave old copies instead of removing from openPQ
+			# Instead of properly updating priorities
+			continue
+
+		openSet.remove(current)
+		closedSet.add(current)
+
+		# If you found the goal, nearly done
+		if (current==goal):
+			return reconstruct(current,cameFrom)
+
+		# Get the neighbors, and for each:
+		neighbors = getneighbors(current)
+		for neighbor in neighbors:
+			# If it isn't already in the closedSet (if it is, ignore it):
+			if neighbor not in closedSet:
+				g = gScore[current]+L2norm(neighbor,current)
+				if neighbor not in openSet:
+					gScore[neighbor] = g
+					openPQ.put((g+heuristic(neighbor,goal),neighbor))
+					openSet.add(neighbor)
+					cameFrom[neighbor]=current
+
+				else: # neighbor is already in open set
+					if g < gScore[neighbor]: # We're doing better with this path; update entry in openPQ
+						gScore[neighbor]=g
+						cameFrom[neighbor] = current
+						# remove old copy won't be necessary because
+						# the lower priority copy will get taken off first and added to open set
+						openPQ.put((g+heuristic(neighbor,goal),neighbor))
+	return False
 
 img = Image.open('map1.png').convert('1')
 imarray = np.array(img)
 
 imgplot = plt.imshow(img)
-print(imarray[99][1])
+
+path = astar((0,10),(51,98))
+
+if (path):
+	print("Found path")
+	xs = [item[0] for item in path]
+	ys = [item[1] for item in path]
+	c_ = xs[::1]
+	plt.scatter(xs,ys, s=10,c=c_,cmap="winter")
+else:
+	print("Didn't find path")
+	
 plt.show()
